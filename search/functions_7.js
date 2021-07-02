77e5b3fd3dbde319fac6960838420367325f0ea7
maede97 <maede97@users.noreply.github.com>
2021-07-02 11:40:21 +0000

    deploy: eb3516791f196e5ce2d71544844466a1d8e3d19a

--- search/functions_7.js
+++ search/functions_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['wait_68',['wait',['../classHPC4WC_1_1Timer.html#a703443ae39250829d4be6129bed274f0',1,'HPC4WC::Timer']]],
-  ['write_69',['write',['../classHPC4WC_1_1IO.html#ab1799a3d43fa098eacddefcd60b289d1',1,'HPC4WC::IO']]]
+  ['write_69',['write',['../classHPC4WC_1_1IO.html#ab1799a3d43fa098eacddefcd60b289d1',1,'HPC4WC::IO::write(std::ostream &amp;stream, const Field &amp;f, Field::const_idx_t &amp;k)'],['../classHPC4WC_1_1IO.html#a2126abb997f25846966b95cc531827fd',1,'HPC4WC::IO::write(std::ostream &amp;stream, const Field &amp;f)']]]
 ];
