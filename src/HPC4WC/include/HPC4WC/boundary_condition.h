1cebb693b84a22e30e95d2632fd904d209732069
Matthias Busenhart <busenham@student.ethz.ch>
2021-07-02 10:22:57 +0200

    Try to add documentation using doxygen

--- src/HPC4WC/include/HPC4WC/boundary_condition.h
+++ src/HPC4WC/include/HPC4WC/boundary_condition.h
@@ -2,9 +2,17 @@
 #include <HPC4WC/field.h>
 
 namespace HPC4WC {
+/**
+ * Periodic boundary conditions, meaning values will be copied over.
+ * @attention Works only on i and j indices.
+ * @todo Customize where to apply the boundary conditions.
+ */
 class PeriodicBoundaryConditions {
 public:
-    // potentially: set periodicity on axis
+    /**
+     * @brief Apply periodic boundary conditions on a field.
+     * @param[inout] field The field to apply the boundary conditions to.
+     */
     static void apply(Field& field);
 };
 
