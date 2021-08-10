f4c03481b6bf7a6784b7727aecdcef0f35ff80ae
maede97 <maede97@users.noreply.github.com>
2021-08-10 16:42:57 +0000

    deploy: c139246214c220c8acb7a35775a634545d4c63fd

--- search/all_e.js
+++ search/all_e.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['scatter_76',['scatter',['../classHPC4WC_1_1Partitioner.html#ae614e8416a2c39e78cf702660954fa9e',1,'HPC4WC::Partitioner']]],
   ['search_77',['search',['../classHPC4WC_1_1AutoTuner.html#a77041692be5a11f6a63e19a0ce3e7fdd',1,'HPC4WC::AutoTuner']]],
-  ['setfrom_78',['setFrom',['../classHPC4WC_1_1Field.html#a185ab1eadd84b630abb8b236f6db5af1',1,'HPC4WC::Field::setFrom(const Field &amp;f)'],['../classHPC4WC_1_1Field.html#a315696d5dadb5d1c44a152fdad345ba8',1,'HPC4WC::Field::setFrom(const Field &amp;f, Field::const_idx_t &amp;offset_i=0, Field::const_idx_t &amp;offset_j=0)'],['../classHPC4WC_1_1Field.html#acd20489468e20f59f15d139c7458690d',1,'HPC4WC::Field::setFrom(const Eigen::MatrixXd &amp;ij_plane_part, const_idx_t &amp;i, const_idx_t &amp;j, const_idx_t &amp;k)']]],
+  ['setfrom_78',['setFrom',['../classHPC4WC_1_1Field.html#a185ab1eadd84b630abb8b236f6db5af1',1,'HPC4WC::Field::setFrom(const Field &amp;f)'],['../classHPC4WC_1_1Field.html#a84526f19172001af1cde6875c13003e8',1,'HPC4WC::Field::setFrom(const Field &amp;f, Field::const_idx_t &amp;offset_i, Field::const_idx_t &amp;offset_j)'],['../classHPC4WC_1_1Field.html#acd20489468e20f59f15d139c7458690d',1,'HPC4WC::Field::setFrom(const Eigen::MatrixXd &amp;ij_plane_part, const_idx_t &amp;i, const_idx_t &amp;j, const_idx_t &amp;k)']]],
   ['simplediffusion_79',['SimpleDiffusion',['../classHPC4WC_1_1SimpleDiffusion.html',1,'HPC4WC']]],
   ['starttime_80',['startTime',['../classHPC4WC_1_1Timer.html#a8a7d9f1119e82d603d7d3c6e53eef784',1,'HPC4WC::Timer']]]
 ];
