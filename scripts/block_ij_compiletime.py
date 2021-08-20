31589709cf584ac328ad50e02efa3fcded011d69
Matthias Busenhart <busenham@student.ethz.ch>
2021-08-20 10:26:43 +0200

    Fix script

--- scripts/block_ij_compiletime.py
+++ scripts/block_ij_compiletime.py
@@ -1,32 +1,28 @@
 import subprocess
 import os
-config = """#pragma once
-namespace HPC4WC {
-#define OPENMP_NUM_THREADS 1
-#define BLOCK_SIZE_I {}
-#define BLOCK_SIZE_J {}
-}  // namespace HPC4WC
-"""
+
+user = "class155"
 
 sizes = [2,4,8,16,32,64,128]
 
 for i in sizes:
     for j in sizes:
-        curr = config.format(i,j)
+        curr = "#pragma once\nnamespace HPC4WC {\n#define OPENMP_NUM_THREADS 1\n#define BLOCK_SIZE_I "
+        curr = curr + str(i) + "\n#define BLOCK_SIZE_J " + str(j) + "\n}  // namespace HPC4WC"
 
         # Write config header
-        with open("~/HPC4WC-Project/src/HPC4WC/include/HPC4WC/config.h", "w") as wr:
+        with open(f"/users/{user}/HPC4WC-Project/src/HPC4WC/include/HPC4WC/config.h", "w") as wr:
             wr.write(curr)
 
         print(f"-blocking-size-i {i} -blocking-size-j {j}")
         # Run cmake
-        os.chdir("~/HPC4WC-Project/build")
+        os.chdir(f"/users/{user}/HPC4WC-Project/build")
         subprocess.run("cmake ..", shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
         subprocess.run("make diffusion2d", shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
 
         # Execute and measure
-        os.chdir("~/HPC4WC-Project/scripts/slurm")
+        os.chdir(f"/users/{user}/HPC4WC-Project/scripts/slurm")
         p = subprocess.run("bash block_ij.sh", shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.PIPE)
-        data = p.stdout
+        data = p.stdout.decode("ascii")
 
         print(data)
